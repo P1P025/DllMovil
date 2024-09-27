7bc57c0c7826327e7e273b936c2e12cbedd54241
KomprasDesarrollo2 <desarrollo2@portal1k.com>
2024-09-27 11:46:05 -0500

    Se agrega mensaje

--- app/src/main/java/com/example/rappi_u/ui/main/ProductViewHolder.kt
+++ app/src/main/java/com/example/rappi_u/ui/main/ProductViewHolder.kt
@@ -2,8 +2,10 @@ package com.example.rappi_u.ui.main
 
 import android.net.Uri
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.rappi_u.R
@@ -14,6 +16,7 @@ class ProductViewHolder(productView: View): RecyclerView.ViewHolder(productView)
     private val weightTextView: TextView = productView.findViewById(R.id.tvProductPrice)
     private val priceTextView: TextView = productView.findViewById(R.id.tvProductWeight)
     private val productImageView:ImageView = productView.findViewById(R.id.ivProductImage)
+    private val btnAddToCart: Button = productView.findViewById(R.id.btnAddToCart)
     fun bind(product: Product){
         val uri = Uri.parse(product.productImageUrl)
         nameTextView.text = product.name
@@ -22,6 +25,9 @@ class ProductViewHolder(productView: View): RecyclerView.ViewHolder(productView)
         Glide.with(productImageView.context)
             .load(uri)
             .into(productImageView)
+        btnAddToCart.setOnClickListener{
+            Toast.makeText(itemView.context, "Se agrego al carrito el producto: " + product.name, Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
